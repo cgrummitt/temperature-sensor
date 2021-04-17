302d8faf6f9f7a651e8191a69b5425662ce6e8b6
cgrummitt <82261384+cgrummitt@users.noreply.github.com>
2021-04-17 10:42:34 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,22 @@
+let voltage = 0
+let temperature = 0
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    basic.showNumber(input.temperature())
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(2000)
+    basic.clearScreen()
+})
 basic.forever(function () {
-	
+    voltage = pins.map(
+    pins.analogReadPin(AnalogPin.P0),
+    0,
+    1023,
+    0,
+    3300
+    )
+    temperature = Math.idiv(voltage - 500, 10)
+    basic.showNumber(temperature)
 })
